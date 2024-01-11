828a680cd6b1455f7c934b49a2ce2eeb78fdf489
Poojitha548 <134190857+Poojitha548@users.noreply.github.com>
2024-01-11 15:59:44 +0530

    Add files via upload

---
+++ Indu B.c
@@ -0,0 +1,8 @@
+//program to find average of 5numbers
+#include<stdio.h>
+int main(){
+         int n1,n2,n3,n4,n5;
+         printf(''enter 5 numbers:'');
+         scanf (''%d%d%d%d%d'',&n1,&n2,&n3,&n4,&n5);
+         printf(''average is :%d'';(n1+n2+n3+n4+n5);/5);
+         }
